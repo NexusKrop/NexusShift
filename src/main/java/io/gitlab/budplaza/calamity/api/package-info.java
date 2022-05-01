ba6594ebc8f3b5f105c2dee1a5849af1ee5bdf2e
WithLithum <WithLithum@foxmail.com>
2022-05-01 10:11:18 +0800

    :fire: Remove proof-of-concept "API"
    These not needed currently.

--- src/main/java/io/gitlab/budplaza/calamity/api/package-info.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2022. All rights reserved.
- * Licensed under GNU Affero GPL, either V3 or any later version.
- */
-
-/**
- * Provides safe access to APIs of Calamity plugins.
- */
-package io.gitlab.budplaza.calamity.api;
