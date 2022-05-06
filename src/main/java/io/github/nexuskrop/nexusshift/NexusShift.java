3e3f07ff03d4ec5e39f4878cb33937826d492558
WithLithum <WithLithum@foxmail.com>
2022-05-06 14:10:17 +0800

    :tada: Rewrite - initial commit

---
+++ src/main/java/io/github/nexuskrop/nexusshift/NexusShift.java
@@ -0,0 +1,13 @@
+package io.github.nexuskrop.nexusshift;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NexusShift extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getLogger().info("Initialisation called");
+
+    }
+}
